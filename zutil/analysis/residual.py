555921e9f0a7c9f6c80c2aeff68f450cc456f30d
Tom Davies <tom.davies@zenotech.com>
2018-07-13 10:37:04 +0100

    Residual Validation Program
    Compares data from two data  files

--- zutil/analysis/residual.py
+++ zutil/analysis/residual.py
@@ -1,44 +1,149 @@
-# practice creating and writing to a csv file
-
 import csv
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+import os
+import os.path
+import pandas as pd
+import operator
 
-##function which takes the new and old variable, and then checks them against a tolerance
+# greatest common divisor function.
+def gcd(a,b):
+    while b > 0:
+        a, b = b, a % b
+    return a
+    
+# lowest common multiple function.
+def lcm(a, b):
+    return a * b / gcd(a, b)
 
-def rescalc(old,new,tol):
-    difference = new - old
-    if abs(difference) <= tol:
+
+# function which opens a data file and returns its data.
+def open_file(file_path,header_TF):
+    if header_TF is True:
+        df = pd.read_csv(file_path, skipinitialspace=True,delim_whitespace=True)
+        return df
+    else:
+        df = pd.read_csv(file_path, skipinitialspace=True,delim_whitespace=True,header = None)
+        return df
+
+
+# function which checks the frequency of the cycles if the cycle column exists and returns it.
+def cycle_freq(df):
+    freq = df.Cycle[3]-df.Cycle[2]
+    return freq
+
+# function which calculates the difference between two numbers and returns it
+def calc(num1,num2,var_diff_temp):
+    diff = float(num1) - float(num2)
+    var_diff_temp.append(diff)
+    return [diff,var_diff_temp]
+    
+# function which checks against a tolerance
+def check_tol(value,tol):
+    if abs(value) <= tol:
         return True
     else:
         return False
+
+
+def main(df,df2,headerTF):
+    num_rows_1 = df.shape[0]
+    num_rows_2 = df2.shape[0]
+    var_diff = []
+    incorrect = []
+    flag = True
     
+        
+    if headerTF is True:
+        max_cycle = min(df.loc[:, 'Cycle'][num_rows_1 - 1],  df.loc[:, 'Cycle'][num_rows_2 - 1])
+            
+        headers = list(df)           
+        cycle_frequency_1 = cycle_freq(df)
+        cycle_frequency_2 = cycle_freq(df2)
+ 
+        
+        cycle_increments = lcm(cycle_frequency_1,cycle_frequency_2)
+        nt = cycle_increments / cycle_frequency_1
+        mt = cycle_increments / cycle_frequency_2 
+        
+        max_iterations_1 = max_cycle / nt
+        max_iterations_2 = max_cycle / mt
+        max_iterations = min(max_iterations_1,max_iterations_2)
+        
+        j = max_iterations / cycle_increments
+        
+        
+        for var in headers:  #selects variable
+            var_diff_temp = []
+            value1 = df.loc[:, var][0]
+            value2 = df2.loc[:, var][0]
+            x = calc(value1,value2,var_diff_temp)
+            diff = x[0]
+            var_diff_temp = x[1]
+            
+            if check_tol(diff,tol[var]) is False:
+                flag = False
+                incorrect.append([df.loc[:, 'Cycle'][0],var])
+            
+            
+            for i in range(1,j+1):
+                value1 = df.loc[:, var][nt*i]
+                value2 = df2.loc[:, var][mt*i]
+                #print [var,df.loc[:, 'Cycle'][i],value1,value2]
+                x = calc(value1,value2,var_diff_temp)
+                diff = x[0]
+                var_diff_temp = x[1]
+                
+                if check_tol(diff,tol[var]) is False:
+                    flag = False
+                    incorrect.append([df.loc[:, 'Cycle'][i],var])
+                    
+            y = max(var_diff_temp, key=abs)
+            var_diff.append(y)
+        
+        
+        if flag is True:
+            print "###############################################\n"
+            print "Validation Passed :-)\n"
+            print "###############################################\n"
+        else:
+            incorrect = sorted(incorrect, key=operator.itemgetter(0))
+            print "###############################################\n"
+            print "Validation Failed :-(\n"
+            print "###############################################\n"
+            print "The validation failed in "+str(len(incorrect))+" places.\n"
+            
+            print "Failure(s) detected at:\n"
+            for j in range(0,len(incorrect)):
+                print "Error: " + str(j+1)
+                print "Cycle: " + str(incorrect[j][0]) + "\nVariable: "+ incorrect[j][1]+"\n"
+            
+            
+            print "###############################################\n"
+            print "Max Differences:\n"
+            for k in range(2,df.shape[1] - 1):
+                print "max "+ headers[k] +" difference: "+ str(var_diff[k])
+            
+            print "\n###############################################"
+            
+################################################################################################################
+        
+headerTF = True  # Set if your data will have headers
 
-#def resdata(oldset,newset,tolerances): ## eg. tolerances = [1, 1, 1e-5, 1e-7, 1e-2, 1e-4, 1e-2, 1e-6, 1e-1]
-    ##open old data
-def resopendata(dataset,p): ## if p is 1 then it returns headers, if 0 returns final cycle        
-    with open(dataset) as f:
-        readCSV = list(csv.reader(f))
-        n = sum(1 for row in readCSV)
-        if p == 0:
-            fin = readCSV[n-1][0].split()
-        elif p == 1:
-            fin = readCSV[0][0].split()
-    return fin
+tol = dict([('RealTimeStep', 1),
+       ('Cycle',1),
+       ('rho',1e-15),
+       ('rhoV[0]',1e-15),
+       ('rhoV[1]',1e-15),
+       ('rhoV[2]',1e-15),
+       ('rhoE',1e-15),
+       ('rhok',1e-15),
+       ('rhoOmega',1e-15)]) #set tolerances for each variable
 
-
-## separate variables and declarations
-def resdata(data1,data2,tolerances):
-    old_data = resopendata(data1,0)
-    new_data = resopendata(data2,0)
-    X = True
-    ## loop over all variables to calculate and returns false if one is outside the tolerance
-
-    for i in range(0,9): #num of var
-        y = rescalc(float(old_data[i]),float(new_data[i]), tolerances[i])
-        if y != True:
-            X = False
-    return X
-
-tolerances = [1, 1, 1e-5, 1e-7, 1e-2, 1e-4, 1e-2, 1e-6, 1e-1]
-resdata('testdata.csv','testdata2.csv',tolerances)
-       
+df = open_file('testdata.csv', headerTF)
+df2 = open_file('testdata2.csv', headerTF)
+        
+main(df,df2,headerTF)
+        
 
